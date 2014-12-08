45ac375e2d77fece1267f4cfc4f19023b6bd6a67
Chuan Xin Lee <cx@bizequity.com>
2014-12-08 06:00:10 -0500

    Added sub-folder for page-specific LESS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
     },
     watch: {
       styles: {
-        files: 'less/*', // which files to watch
+        files: 'less/**/*', // which files to watch
         tasks: ['less'],
         options: {
           nospawn: true
